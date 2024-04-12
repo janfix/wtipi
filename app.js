24b56bfa7260f4c1e8096f4886525578c77ac758
janfix <jean.philippe.riviere@gmail.com>
2024-04-12 09:52:57 +0200

    upload is functionnal

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const mongoose = require("mongoose");
 const blogRoutes = require("./routes/blogRoutes");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const AdmZip = require("adm-zip");
+const fs = require("fs");
+const path = require("path");
 const { checkUser } = require("./middleware/authMiddleware");
 
 const app = express();
+
+// Configuration de Multer
+const upload = multer({ dest: 'upload/' });
 
 const dbURI = process.env.DB_URI;
 
@@ -15,15 +22,14 @@ mongoose
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-// register view engine
 app.set("view engine", "ejs");
 
-// middleware and static files
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Insérer ici la vérification de l'utilisateur pour toutes les routes
 app.get("*", checkUser);
 
 app.get("/", (req, res) => {
@@ -32,6 +38,50 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// Route pour télécharger le fichier ZIP
+app.post('/upload', upload.single('file'), (req, res) => {
+  // Vérifie si un fichier a été téléchargé
+  if (!req.file) {
+    console.log('Route /upload atteinte');
+    console.log(req.file)
+    return res.status(400).send('No file uploaded.');
+  } else{
+    console.log("MAIS OUUUUIIIIII Bravo!!!!")
+    try {
+      // Chemin où le fichier ZIP téléchargé est temporairement stocké
+      const tempPath = req.file.path;
+  
+      // Créer une instance de AdmZip
+      const zip = new AdmZip(tempPath);
+      const zipEntries = zip.getEntries(); // Un tableau des entrées (fichiers/dossiers) du ZIP
+  
+      // Validation du contenu du ZIP (exemple simple)
+     /*  const isValid = zipEntries.every(entry => {
+        // Ici, vous pouvez ajouter des conditions de validation spécifiques,
+        // par exemple vérifier les types de fichiers, les noms, etc.
+        return !entry.isDirectory && path.extname(entry.entryName) === '.txt';
+      });
+  
+      if (!isValid) {
+        return res.status(400).send('Invalid ZIP content.');
+      } */
+  
+      // Si valide, copiez le fichier ZIP à l'emplacement désiré
+      const targetPath = path.join(__dirname, 'destination', 'test123456789.zip');
+      console.log("UPLOAD CHECK!")
+      fs.copyFileSync(tempPath, targetPath);
+  
+      res.send('File uploaded and validated successfully.');
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Server error.');
+    } 
+  }
+
+/*  */
+
 });
 
 app.use("/blogs", blogRoutes);
